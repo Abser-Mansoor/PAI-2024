824a073b549403c5bcab92737ad984353da5f68c
Abser-Mansoor <fbsefigbhgnrihg@outlook.com>
2024-08-21 22:02:06 +0500

    Update Task5.py

--- Labs/Lab01/Task5.py
+++ Labs/Lab01/Task5.py
@@ -1,1 +1,10 @@
-a
+lis = []
+num = int(input('Enter the length of list: '))
+lim = int(input('Enter the limit: '))
+
+for i in range(num):
+    lis.append(int(input('Enter the number: ')))
+
+lis = [x for x in lis if x >= lim]
+
+print("Modified list: ", lis)
