18814362663cd5bec6c9361dadb576abd34cf499
Abser-Mansoor <fbsefigbhgnrihg@outlook.com>
2024-09-04 09:05:55 +0500

    Update Task3.py

--- Labs/Lab03/Task3.py
+++ Labs/Lab03/Task3.py
@@ -1,1 +1,19 @@
-t
+try:
+    # tanks = ["PzKmpfWg IV", "Jagdpanzer 38(t)", "PzKmpfWg VI Konig Tiger"]
+    # times = ["1938", "1939", "1942"]    
+    plane = [item.strip() for item in input("Enter names of aircraft separated by commas ").split(',')]
+    times = [item.strip() for item in input("Enter times at which production started: ").split(',')]
+    
+    dictionary = {}
+
+    for time,planes in zip(times,plane):
+        dictionary[planes] = time
+
+    with open(r'text3.txt','w') as fileobj:
+        fileobj.write(str(dictionary))
+except FileNotFoundError:
+    print("File not Found")
+except IOError:
+    print("IOError occured")
+except Exception as error:
+    print("Unexpected Error Occured") 
