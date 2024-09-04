2f715178c825d7ec02c4a75b9c96f7f9656f9422
Abser-Mansoor <fbsefigbhgnrihg@outlook.com>
2024-09-04 08:45:50 +0500

    Update Task1.py

--- Labs/Lab03/Task1.py
+++ Labs/Lab03/Task1.py
@@ -1,4 +1,13 @@
-with open(r'text.txt') as fileobj:
+try:    
+    with open(r'text.txt') as fileobj:
 
-    string = fileobj.read()
-    print(f"Number of characters in file is: {len(string)}")
+        string = fileobj.read()
+        count = 1
+        print(f"Number of characters in file is: {len(string)}")
+        print(f"Number of words in file is: {len(string.split())}")
+except FileNotFoundError:
+        print("File not Found")
+except IOError:
+    print("IOError Occured")
+except Exception as error:
+    print("Unexpected error")
