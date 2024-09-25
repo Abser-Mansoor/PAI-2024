97d5ad93f85c0ec3e5d548081ea90c4c49abde2e
Abser-Mansoor <fbsefigbhgnrihg@outlook.com>
2024-09-25 09:04:05 +0500

    Create Task4.py

---
+++ Labs/Lab6/Task4.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+Dataset = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael',
+    'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, None, 9, 20, 14.5, None, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes'],
+}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+series1 = pd.DataFrame(data=Dataset)
+series1.index = pd.RangeIndex(1,1 + len(labels))
+print(series1)
