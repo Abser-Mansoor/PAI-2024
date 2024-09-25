6f25ffcd5ec909669cff62b0bade8b1d7326454d
Abser-Mansoor <fbsefigbhgnrihg@outlook.com>
2024-09-25 08:46:40 +0500

    Create Task1.py

---
+++ Labs/Lab6/Task1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+Revenue = {
+    "Movies": ["Look who\'s Back","Downfall","Stalingrad"],
+    "Revenue": [3000000,1000000,6000000],
+    "Budget": [999999,2000000,3000000]
+}
+
+series1 = pd.DataFrame(Revenue).set_index('Movies')
+modified = series1[(series1['Revenue'] > 2000000) & (series1['Budget'] < 1000000)]
+print(modified)
