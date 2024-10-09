df245ed3f712131841051e5761b2fd0272775ca5
Abser-Mansoor <fbsefigbhgnrihg@outlook.com>
2024-10-09 09:17:52 +0500

    Update Task5.py

--- Labs/Lab08/Task5.py
+++ Labs/Lab08/Task5.py
@@ -3,3 +3,4 @@ import numpy as np
 odd_mat = np.array(np.arange(1,33,2)).reshape(4,4)
 random_mat = np.random.choice([2,5,9,10],size=(4,4))
 print(np.matmul(odd_mat,np.matmul(random_mat,np.eye(4,4))))
+print(random_mat*odd_mat*np.eye(4,4))
