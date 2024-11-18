b5104a16aa903848bb443b25754f561722c1ef34
Abser-Mansoor <fbsefigbhgnrihg@outlook.com>
2024-11-18 11:03:16 +0500

    Update Task1.py

--- Labs/Lab11/Task1.py
+++ Labs/Lab11/Task1.py
@@ -1,5 +1,4 @@
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, precision_score
